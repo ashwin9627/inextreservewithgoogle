4a4b53034810afa617568c95e8609c5fe13110a8
Your Name <you@example.com>
2022-07-27 18:25:10 +0530

    merchant upate

--- ReserveWithGoogle/Controllers/ReserveWithGoogleController.cs
+++ ReserveWithGoogle/Controllers/ReserveWithGoogleController.cs
@@ -28,59 +28,67 @@ namespace ReserveWithGoogle.Controllers
         {
             try
             {
-                // Merchant name from read from App Settings
-                string merchantName = "reservation";
                 //Read a merchant details json
                 var rootPath = _hostingEnvironment.ContentRootPath; //get the root path
-                var fullPath = Path.Combine(rootPath, "Json/Merchent.json"); //combine the root path with that of our json file inside mydata directory
-                var jsonData = System.IO.File.ReadAllText(fullPath); //read all the content inside the file
-                
-                //object formed from merchant json
-                Merchent merchant = JsonConvert.DeserializeObject<Merchent>(jsonData.ToString());
-                //Filtered matched merchant name with provided merchant name.
-                Merchant filterMerchant = merchant.merchants.merchant.Where(x => x.service_id == merchantName).FirstOrDefault();
-                Availabilityclass availabilityFilecontent = new Availabilityclass();
+                //var fullPath = Path.Combine(rootPath, "Json/Merchent.json"); 
+                var fullPath = Path.Combine(rootPath, "Json/Merchent1.json"); //combine the root path with that of our json file inside mydata directory
+                var jsonData = System.IO.File.ReadAllText(fullPath); //read all the content inside the file                
 
-                //Filtere result has data
-                if(filterMerchant!=null)
+                //object formed from merchant json                
+                Merchants merchants = JsonConvert.DeserializeObject<Merchants>(jsonData.ToString());
+                List<string> MerchantList =new List<string>();// storing the merchantName
+
+                //Looping mechants and create json file for each merchant
+                foreach (var merchantname in merchants.merchants)
                 {
-                    DateTime dateTimeHR = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 11, 0, 0);
-                    int curTimeStamp = Convert.ToInt32(new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds());
-                    int curTimeStampHR = Convert.ToInt32(new DateTimeOffset(dateTimeHR).ToUnixTimeSeconds());
-                    availabilityFilecontent.metadata = new Metadata();
-                    availabilityFilecontent.metadata.processing_instruction = "PROCESS_AS_COMPLETE";
-                    availabilityFilecontent.metadata.shard_number = 0;
-                    availabilityFilecontent.metadata.total_shards = 1;
-                    availabilityFilecontent.metadata.nonce = "9371383477";
-                    availabilityFilecontent.metadata.generation_timestamp = curTimeStampHR;//Convert.ToInt64(DateTime.Now.ToFileTime());//1652193212;
+                    //Filtered matched merchant name with provided merchant name.
+                    Merchant filterMerchant = merchants.merchants.Where(x => x.service_id == merchantname.service_id).FirstOrDefault();
+                    Availabilityclass availabilityFilecontent = new Availabilityclass();
 
-                    availabilityFilecontent.service_availability = new List<ServiceAvailability>();
-                    List<Availability> availabilityList = new List<Availability>();
-                    // looping with number of restaurents from same merchant
-                    foreach (var x in filterMerchant.details)
+                    //Filtere result has data
+                    if (filterMerchant != null)
                     {
-                                List<Availability> availabilityList1 = new List<Availability>();
-                                availabilityList.Add(new Availability
-                                {
-                                    service_id = filterMerchant.service_id,
-                                    spots_open = x.open_slots,
-                                    spots_total = x.total_slots,
-                                    confirmation_mode = "CONFIRMATION_MODE_SYNCHRONOUS",
-                                    duration_sec = 3600,
-                                    resources = new Resources { party_size = x.party_size }
-                                });
-                                
-                                //curTimeStampHR = curTimeStampHR + 3600;
-                            }
-                    availabilityFilecontent.service_availability.Add(new ServiceAvailability { availability = availabilityList });
-                }
-                //serializing object to json string an store into the json file
-                var json = JsonConvert.SerializeObject(availabilityFilecontent, Formatting.Indented);
-                System.IO.File.WriteAllText(@"Json/CreateJson.json", json);
+                        DateTime dateTimeHR = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 11, 0, 0);
+                        int curTimeStamp = Convert.ToInt32(new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds());
+                        int curTimeStampHR = Convert.ToInt32(new DateTimeOffset(dateTimeHR).ToUnixTimeSeconds());
+                        availabilityFilecontent.metadata = new Metadata();
+                        availabilityFilecontent.metadata.processing_instruction = "PROCESS_AS_COMPLETE";
+                        availabilityFilecontent.metadata.shard_number = 0;
+                        availabilityFilecontent.metadata.total_shards = 1;
+                        availabilityFilecontent.metadata.nonce = "9371383477";
+                        availabilityFilecontent.metadata.generation_timestamp = curTimeStampHR;//Convert.ToInt64(DateTime.Now.ToFileTime());//1652193212;
 
-                //Send the file through ftp
-                //sendfile(@"Json/CreateJson.json");
-                return null;
+                        availabilityFilecontent.service_availability = new List<ServiceAvailability>();
+                        List<Availability> availabilityList = new List<Availability>();
+                        // looping with number of restaurents from same merchant
+                        foreach (var x in filterMerchant.details)
+                        {
+                            
+                            availabilityList.Add(new Availability
+                            {
+                                service_id = filterMerchant.service_id,
+                                spots_open = x.open_slots,
+                                spots_total = x.total_slots,
+                                confirmation_mode = "CONFIRMATION_MODE_SYNCHRONOUS",
+                                duration_sec = 3600,
+                                resources = new Resources { party_size = x.party_size }
+                            });
+                            
+                            //curTimeStampHR = curTimeStampHR + 3600;
+                        }                        
+                        availabilityFilecontent.service_availability.Add(new ServiceAvailability { availability = availabilityList });
+                        
+                    }
+                    MerchantList.Add(merchantname.service_id);
+                    //serializing object to json string an store into the json file
+                    var json = JsonConvert.SerializeObject(availabilityFilecontent, Formatting.Indented);
+                    System.IO.File.WriteAllText(@"Availability/" + merchantname.service_id+".json", json);
+                    //Send the file through ftp
+                    //sendfile(@"Json/CreateJson.json");
+                    //return json;
+                }
+
+                return "";
             }
             catch (Exception ex)
             {
---
+++ ReserveWithGoogle/Model/Merchants.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReserveWithGoogle.Model
+{    
+    public class Detail
+    {
+        public int party_size { get; set; }
+        public int total_slots { get; set; }
+        public int open_slots { get; set; }
+    }
+
+    public class Merchant
+    {
+        public string name { get; set; }
+        public string service_id { get; set; }
+        public List<Detail> details { get; set; }
+    }
+
+    public class Merchants
+    {
+        public List<Merchant> merchants { get; set; }
+    }
+}
